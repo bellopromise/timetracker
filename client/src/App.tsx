469e2cc1ed9eb3587b161f112ad7f05521922230
Bello Promise <bellopromise1997@gmail.com>
2023-06-05 04:34:15 +0100

    21 - Modified App.tsx to input routers for the components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import CreateAccountPage from './Components/CreateAccountPage';
+import LoginPage from './Components/LoginPage';
+import PrivateRoute from './Components/PrivateRoute';
+import WelcomePage from './Components/WelcomePage';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route  path="/login" element={<LoginPage/>} />
+        <Route path="/create-account" element={<CreateAccountPage/>} />
+        
+        <Route path="/" element={<PrivateRoute><WelcomePage/></PrivateRoute>} />
+        
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
