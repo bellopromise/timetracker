b6700251e4cee6344b8c38d4f7eba1b2f49d24bb
Bello Promise <bellopromise1997@gmail.com>
2023-06-05 04:36:10 +0100

    AModified index.tsx to have all the components and implement all providers

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,17 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React, { useReducer } from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'tailwindcss/tailwind.css';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { UserProvider } from './Components/UserContext';
+import AppProvider from './Components/AppProvider';
+import { AppContext, appReducer, initialState } from './Components/reducer';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const client = new ApolloClient({
+  uri: 'http://localhost:3000/graphql', // Replace with your GraphQL endpoint
+  cache: new InMemoryCache(),
+});
+
+function Root() {
+  const [state, dispatch] = useReducer(appReducer, initialState);
+  const appContextValue = { state, dispatch };
+
+  return (
+    <React.StrictMode>
+      <AppProvider>
+        <UserProvider>
+          <AppContext.Provider value={appContextValue}>
+            <ApolloProvider client={client}>
+              <App />
+            </ApolloProvider>
+          </AppContext.Provider>
+        </UserProvider>
+      </AppProvider>
+    </React.StrictMode>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<Root />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
